50c77b44a7e8cf92183d86f800361d1264a5b30d
clickgirish <clickgirish@gmail.com>
2023-03-24 23:56:29 +0530

    Alignment fixed

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <Header title="SINDHIONISM.ORG" />
         <Header title="SINDHIONISM.CO.IN" />
         <p className="description">
-          Get each domain for just $2499! <br /> OR <br />Get a bundle deal for just $5499!
+          Get each domain for just $2499! <br /><center> OR </center><br />Get a bundle deal for just $5499!
         </p>
         <p>
           Contact now and grab the deal!<br /><a href="mailto:sales@iotechnolabs.com">sales@iotechnolabs.com</a>
