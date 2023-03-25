26a58f3be5850ce05b9e0fd212d68edcc07e321a
NakshChawrai <clickgirish@gmail.com>
2023-03-25 23:26:04 +0530

    Updated hyperlinks

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <a href="sindhionism.com" title="It all starts with a name!"><Header title="SINDHIONISM.COM" /></a>
-        <a href="sindhionism.org" title="It all starts with a name!"><Header title="SINDHIONISM.ORG" /></a>
-        <a href="sindhionism.co.in" title="It all starts with a name!"><Header title="SINDHIONISM.CO.IN" /></a>
+        <a href="www.sindhionism.com" title="It all starts with a name!"><Header title="SINDHIONISM.COM" /></a>
+        <a href="www.sindhionism.org" title="It all starts with a name!"><Header title="SINDHIONISM.ORG" /></a>
+        <a href="www.sindhionism.co.in" title="It all starts with a name!"><Header title="SINDHIONISM.CO.IN" /></a>
         <p className="description">
           Get each domain for just $2499! <br /><center> OR </center><br />Get a bundle deal for just $5499!
         </p>
